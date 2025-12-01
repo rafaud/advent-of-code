c455da11e6b7208c8a1eb7e96e36cd405cd3c657
rafaud <rafauds@icloud.com>
2025-12-01 21:40:32 +0100

    Created better solution for day1,2025 with modulo and commented it

--- 2025/day01/main.py
+++ 2025/day01/main.py
@@ -41,35 +41,71 @@ def main():
         if args.verbose:
             print(f"{index}. The dial will be rotated {"R" if rotation > 0 else "L"}{abs(rotation)} from {position}.")
 
-        # Brute Force
-        # TODO: Make a better version of this
+        # Calculate:
+        # new_position_relative - position on infinite dial with negative numbers
+        # new_position_absolute - position on 0 - 99 dial
+        # full_rotations - how many full rotations there were
 
-        if position == 0 and rotation < 0:
-            position += DIAL_SIZE
+        new_position_relative = position + rotation
+        full_rotations, new_position_absolute = divmod(new_position_relative, DIAL_SIZE)
 
-        position += rotation
-        while position > DIAL_SIZE:
+        if args.verbose:
+            print(f"\tFull rotations: {abs(full_rotations)}")
+
+        # Modulo "hides" crossing exactly zero, but only for 0 itself and negative numbers so we need to add that one rotation
+        # Start at 50; rotate 100 left; land on -50; divmod(-50, 100)=(-1, 50); One crossing (abs(-1)) and land on 50.      Correct answer
+        # Start at 50; rotate 150 left; land on -100; divmod(-100, 100)=(-1, 0); One crossing (abs(-1)) and land on 0.      Incorrect answer
+        # Start at 50; rotate 151 left; land on -101; divmod(-101, 100)=(-2, 99); Two crossings (abs(-2)) and land on 99.   Correct answer
+        if new_position_absolute == 0 and new_position_relative <= 0:
             if args.verbose:
-                print("\tOver 100, one 0 crossing")
-            position -= DIAL_SIZE
-            zeros_counter += 1
-        if position == DIAL_SIZE:
-            position = 0
-
-        while position < 0:
-            if args.verbose:
-                print("\tUnder 100, one 0 crossing")
-            position += DIAL_SIZE
+                print(f"\tLanded on zero.")
             zeros_counter += 1
 
-        if position == 0:
+        # Starting rotation to the left from zero results in one additional "crossing" due to the nature of modulo, we need to eliminate this
+        # Start at 0; rotate 2 left; land on -2; divmod(-2, 100)=(-1, 98); One crossing (abs(-1)) and land on 98.           Incorrect answer
+        if position == 0 and new_position_relative < 0:
             if args.verbose:
-                print("\tLanded on 0")
-            zeros_counter += 1
+                print(f"\tStarted left rotation from zero.")
+            zeros_counter -= 1
+
+        # Add full rotations and update position
+        zeros_counter += abs(full_rotations)
+        position = new_position_absolute
+
+        # Part 1, just count landings on zero
+        if new_position_absolute == 0:
+            if args.verbose:
+                print(f"\t[P1] Landed on zero.")
             zero_stops += 1
+
+        # Brute Force
+        # if position == 0 and rotation < 0:
+        #     position += DIAL_SIZE
+        #
+        # position += rotation
+        # while position > DIAL_SIZE:
+        #     if args.verbose:
+        #         print("\tOver 100, one 0 crossing")
+        #     position -= DIAL_SIZE
+        #     zeros_counter += 1
+        # if position == DIAL_SIZE:
+        #     position = 0
+        #
+        # while position < 0:
+        #     if args.verbose:
+        #         print("\tUnder 100, one 0 crossing")
+        #     position += DIAL_SIZE
+        #     zeros_counter += 1
+        #
+        # if position == 0:
+        #     if args.verbose:
+        #         print("\tLanded on 0")
+        #     zeros_counter += 1
+        #     zero_stops += 1
 
         if args.verbose:
             print(f"The dial was rotated {"R" if rotation > 0 else "L"}{abs(rotation)} to point at {position}, total zero crossings: {zeros_counter}.\n")
+    # Results should be 1026 and 5923
     print(f"Dial stopped at 0 total of {zero_stops} times.")
     print(f"Dial pointed at 0 total of {zeros_counter} times.")
 
